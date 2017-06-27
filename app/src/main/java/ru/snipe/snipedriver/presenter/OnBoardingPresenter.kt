c0e0f371496c788ec5f42aab7c31d44c5a64ee2b
Egor Danilin <edanilin@techops.ru>
2017-06-27 14:05:44 +0300

    Changed menu item text color

--- app/src/main/java/ru/snipe/snipedriver/presenter/OnBoardingPresenter.kt
+++ app/src/main/java/ru/snipe/snipedriver/presenter/OnBoardingPresenter.kt
@@ -1,7 +1,6 @@
 package ru.snipe.snipedriver.presenter
 
 import com.hannesdorfmann.mosby3.mvi.MviBasePresenter
-import io.reactivex.Observable
 import ru.snipe.snipedriver.data.DataManager
 import ru.snipe.snipedriver.view.onboarding.OnBoardingView
 import javax.inject.Inject
@@ -9,11 +8,6 @@ import javax.inject.Inject
 class OnBoardingPresenter
 @Inject constructor(var dataManager: DataManager) : MviBasePresenter<OnBoardingView, OnBoardingViewState>() {
     override fun bindIntents() {
-        val sub: Observable<LoginViewState>
-                = intent { it.loginButtonClicked() }
-                .map { dataManager.login(); LoginViewState.LoggedInViewState() }
-
-//        subscribeViewState(sub, OnBoardingView::render)
     }
 }
 
